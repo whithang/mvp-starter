a60cbf4d61c521b8961c4d466b4a70bd40932946
Kelly Whiting <kwhiting@gmail.com>
2017-08-04 16:53:41 -0700

    added new user entry form

--- react-client/src/components/NewUser.jsx
+++ react-client/src/components/NewUser.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+// create entry form for all db fields & courses, multi option
+// on submit, ensure not duplicate and alert error you already exist
+// make state a dropdown field
+// restrict size of input in fields and validity of email and int for handicap
+// make password input be encrypted
 const NewUser = (props) => (
   <div>
     <form>
-      // create entry form for all db fields & courses, multi option
-      // on submit, ensure not duplicate and alert error you already exist
+      <label>Email / Login: <input type='text' name='email' /></label>
+      <label>Password: <input type='text' name='password' /></label>
+      <br></br>
+      <label>First Name: <input type='text' name='first_name' /></label>
+      <label>Last Name: <input type='text' name='last_name' /></label>
+      <br></br>
+      <label>Phone: <input type='text' name='phone' /></label>
+      <label>City: <input type='text' name='city' /></label>
+      <label>State: <input type='text' name='state' /></label>
+      <br></br>
+      <label>Golf Handicap: <input type='text' name='handicap' /></label>
+      <input type='submit' value='Submit' />
     </form>
   </div>
 )
